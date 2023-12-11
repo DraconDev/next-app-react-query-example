73483e9727bf73cbed957c9e79d219bff21db8cb
DraconDev <dracsharp@gmail.com>
2023-12-11 20:24:20 +0000

    add basic example

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-
 export default function Home() {
-  return (
-    <main></main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between w-full">
+            <h1>Home</h1>
+            <div className="w-full">
+                <h1>Pages</h1>
+            </div>
+        </main>
+    );
 }
